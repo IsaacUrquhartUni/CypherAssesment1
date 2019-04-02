eb417955a1f8039ef62bacc67c9d8624075bbaa1
Isaac Urquhart <isaac.urquhart@uon.edu.au>
2019-04-02 22:33:13 +0000

    Created menu and added blank rotationEncryption.c file

--- main.c
+++ main.c
@@ -2,5 +2,15 @@
 
 int main() 
 {
-  return 0;
+    printf("############################################################\n");
+    printf("# Please select an option from the menu below:             #\n");
+    printf("#                                                          #\n");
+    printf("# (a) Rotation encryption      (b) Rotation decryption     #\n");
+    printf("# (c) Substitution encryption  (d) Substitution decryption #\n");
+    printf("#                                                          #\n");
+    printf("############################################################\n");
+    
+    return 0;
 }
+
+
